0e3af3feac4d73997a66d299d8527c4dee2bf170
Márcio Boaro <marcio.boaro@gmail.com>
2021-11-06 13:32:20 -0300

    Mudança de Servidor

--- config/db.py
+++ config/db.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine, MetaData
 
-user = "remoteuser"
-passw = "new_password"
+user = "admin"
+passw = "V8Awdu2PdN7P6QLv"
 port = 3306
 base = "rede1minuto"
-host = "rede1min.eastus2.cloudapp.azure.com"
+host = "database-1.czsak8q16ylr.us-east-1.rds.amazonaws.com"
 charset = "utf8"
 
 str_conn = "mysql+pymysql://"+str(user)+":"+str(passw)+"@"+str(host)+":"+str(port)+"/"+str(base)+"?charset="+charset
