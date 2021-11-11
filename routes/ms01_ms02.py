df0e2daabfbe47d9cddf32ec0f286dab00a4cab1
Márcio Boaro <marcio.boaro@gmail.com>
2021-11-11 15:50:01 -0300

    tirar : da estação locker - MS01

--- routes/ms01_ms02.py
+++ routes/ms01_ms02.py
@@ -248,7 +248,7 @@ def ms01(ms01: MS01, public_id=Depends(auth_handler.auth_wrapper)):
             Locker['Feriados_Hora_Inicio'] = record[28]
             Locker['Feriados_Hora_Fim'] = record[29]
             Estacao_Locker.append(Locker)
-        ms02['Estacao_Locker:'] = Estacao_Locker  
+        ms02['Estacao_Locker'] = Estacao_Locker  
         return ms02
     except:
         logger.error(sys.exc_info())
