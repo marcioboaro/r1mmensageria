0efb228daeb510540602060a29ebf0b52bff4c88
André Monteiro <monteiro.hat@gmail.com>
2022-04-24 13:51:36 -0300

    Merge remote-tracking branch 'origin/homolog'

--- config/db.py
+++ config/db.py
@@ -1,10 +1,23 @@
 from sqlalchemy import create_engine, MetaData
+from config.config import ConfigServer
 
+<<<<<<< HEAD
+config = ConfigServer()
+
+database_config = config.get_database_config()
+
+user = database_config["USER"]
+passw = database_config["PASSWORD"]
+port = database_config["PORT"]
+base = database_config["BASE"]
+host = database_config["HOST"]
+=======
 user = "rede1minuto"
 passw = "k9X1wj2UEj5FD5G2H4pl"
 port = 3306
 base = "rede1minuto"
 host = "dbprod.rede1minuto.com.br"
+>>>>>>> origin/homolog
 charset = "utf8"
 
 str_conn = "mysql+pymysql://"+str(user)+":"+str(passw)+"@"+str(host)+":"+str(port)+"/"+str(base)+"?charset="+charset
@@ -12,3 +25,5 @@ engine = create_engine(str_conn, pool_size=20, max_overflow=100, execution_optio
 
 meta = MetaData()
 conn = engine.connect()
+
+#mudei
